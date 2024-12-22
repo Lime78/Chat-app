0386e2d2e0676da831a3140e3c2fbd2c83f81423
Liam Bard <liam.axel.bard@gmail.com>
2024-12-22 13:41:00 +0100

    login klart kanaler klart, message

--- backendSrc/routes/messages.ts
+++ backendSrc/routes/messages.ts
@@ -1,29 +1,3 @@
-// import express, { Request, Response } from 'express';
-// import { getMessagesByChannel } from '../database/messages.js';
-// import { ObjectId } from 'mongodb';
-
-// const router = express.Router();
-
-// router.get('/:channelId', async (req: Request, res: Response) => {
-//     const { channelId } = req.params;
-
-//     try {
-//         if (!ObjectId.isValid(channelId)) {
-//              res.status(400).json({ message: 'Invalid channel ID' });
-// 			 return
-//         }
-
-//         const channelObjectId = new ObjectId(channelId);
-//         const messages = await getMessagesByChannel(channelObjectId);
-//         res.json(messages);
-//     } catch (error) {
-//         console.error('Error fetching messages:', error);
-//         res.status(500).json({ message: 'Internal server error' });
-//     }
-// });
-
-// export { router };
-
 import express, { Request, Response, Router } from 'express'
 import { WithId, ObjectId } from 'mongodb'
 import { Message } from '../models/messages.js'
--- backendSrc/server.ts
+++ backendSrc/server.ts
@@ -2,7 +2,10 @@
 import express, { Express, Request, Response, NextFunction } from 'express'
 import { router as userRouter } from './routes/users.js'
 import { router as channelRouter } from './routes/channels.js'
+import { router as messagesRouter } from './routes/messages.js'
+import { authenticateToken } from './routes/authMiddleware.js'
 import { router as loginRouter } from './routes/login.js'
+import { router as authRouter } from './routes/auth.js'
 
 const app: Express = express()
 const port: number = Number(process.env.PORT || 4242)
@@ -15,12 +18,14 @@ app.use('/', (req: Request, __: Response, next: NextFunction) => {
   console.log(`${req.method} ${req.url}`, req.body)
   next()
 })
+app.use('/api/auth', authRouter)
 
-app.use('/api/users', userRouter)
+app.use('/api/userInfo', userRouter)
 app.use('/api/channels', channelRouter)
-app.use('/api/login', loginRouter)
+app.use('/api/messages', authenticateToken, messagesRouter)
+// app.use('/api/login', loginRouter)
 
 // Starta servern
 app.listen(port, () => {
-  console.log('Server is listening on port ' + port)
+  console.log(`Server is listening on port ${port}`)
 })
